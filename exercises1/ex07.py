9241856610342ce9e6d58b15c7a687e52dc26f71
Dmitri Calle <glizqc06@gmail.com>
2025-10-30 16:24:01 -0300

    create ex07.py

---
+++ exercises1/ex07.py
@@ -0,0 +1,10 @@
+print('Esta é a sua calculadora de divisão online! Preencha os campos abaixo e descubra o resultado.')
+print('')
+
+num1 = int(input('Digite o primeiro número (ele será o dividendo): '))
+num2 = int(input('Digite o segundo número (ele será o divisor): '))
+
+resultado = num1/num2
+
+print('')
+print('O resultado dessa divisão é: ',resultado)

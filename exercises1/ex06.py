5348bf404569bde8e4c6b8da6755caeaa948c477
Dmitri Calle <glizqc06@gmail.com>
2025-10-30 16:22:54 -0300

    create ex06.py
    implement a program to calculate clothing combinations

---
+++ exercises1/ex06.py
@@ -0,0 +1,10 @@
+print('Este é um programa para calcular combinações de roupas (calças e camisetas)')
+print('')
+
+calca = int(input('Número de calças: '))
+camiseta = int(input('Número de camisetas: '))
+
+total = calca * camiseta
+
+print('')
+print('As combinações possíveis são: ', total)

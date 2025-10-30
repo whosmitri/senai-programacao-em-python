4e7b0c4f115ea17aee2f7be4c03899c82b962e0c
Dmitri Calle <glizqc06@gmail.com>
2025-10-30 16:21:53 -0300

    create ex10.py

---
+++ exercises1/ex10.py
@@ -0,0 +1,8 @@
+print('Este é um programa para somar as notas dos alunos, "Nota da prova + Nota do trabalho"')
+
+Nota1 = int(input("Nota da prova: "))
+Nota2 = int(input("Nota do trabalho: "))
+
+NotaFinal = Nota1 + Nota2
+
+print('A nota total do aluno é: ', NotaFinal)

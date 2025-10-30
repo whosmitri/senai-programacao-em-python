1b6efd1846e4b7eaaf1f216c5cf7bc28a0e0c482
Dmitri Calle <glizqc06@gmail.com>
2025-10-30 16:24:48 -0300

    create ex08.py
    add program to calculate spending on a purchase

---
+++ exercises1/ex08.py
@@ -0,0 +1,12 @@
+print('Este é um programa para calcular o seu gasto em uma única compra.')
+print('')
+
+#v = valor
+
+vInicial = int(input('Valor inicial: '))
+vFinal = int(input('Valor final: '))
+
+gasto = vInicial - vFinal
+
+print('')
+print('Você gastou R$', gasto, '(reais)')
